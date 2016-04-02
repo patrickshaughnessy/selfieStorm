0be6640a796e535be20100fca9acc2e9d3d4b5cf
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-04-02 15:08:48 -0700

    initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,63 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $cordovaCamera) {
+
+
+  $scope.takePhoto = function(){
+    console.log('take a selfie');
+
+    document.addEventListener("deviceready", function () {
+
+      var options = {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 100,
+        targetHeight: 100,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false,
+  	    correctOrientation:true
+      };
+
+      $cordovaCamera.getPicture(options).then(function(imageData) {
+        // var image = document.getElementById('myImage');
+        // image.src = "data:image/jpeg;base64," + imageData;
+
+        // 
+
+      }, function(err) {
+        // error
+      });
+
+    }, false);
+
+  }
+
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
